f820c8ab45e81a68ebb3d264a5ae18166cc50ee8
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-07-18 13:17:34 +0200

    created NavbarComponent to show nav on main

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <nav>
+        <div class="d-flex justify-content-between  ">
+            <div>
+                <span><i class="fa-solid fa-bars"></i></span>
+            </div>
+            <div>
+                <span>Navbar</span>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+nav{
+    width: 100%;
+}
+</style>
