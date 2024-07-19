fdc60db3568dd08c050f9267ad00063336c36654
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-07-19 10:22:46 +0200

    added name icon and mail in navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,11 +1,13 @@
 <template>
     <nav>
-        <div class="d-flex justify-content-between  ">
+        <div class="d-flex justify-content-between p-3">
             <div>
                 <span><i class="fa-solid fa-bars"></i></span>
             </div>
             <div>
-                <span>Navbar</span>
+                <span><i class="fa-regular fa-envelope me-3"></i></span>
+                <img class="ldg-img" src="../assets/img/user-icon.png">
+                <span>Mario Rossi</span>
             </div>
         </div>
     </nav>
@@ -20,5 +22,11 @@
 <style lang="scss" scoped>
 nav{
     width: 100%;
+}
+
+.ldg-img{
+    width: 45px;
+    border-radius: 50%;
+    border: 1.5px solid white;
 }
 </style>
